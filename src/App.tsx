357fd202b0f57db86ce31f8ec9ffb8856a11a073
Ishaan Puniani <ishaan_puniani@yahoo.com>
2026-06-06 19:10:59 +0000

    fixed routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,29 @@
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route, useSearchParams, useNavigate } from 'react-router-dom';
 import Layout from './components/Layout';
 import Home from './pages/Home';
 import Tours from './pages/Tours';
 import Demos from './pages/Demos';
 import Tooltips from './pages/Tooltips';
+import { useEffect } from 'react';
+
+const baseName = "/pagepilot-vanilla-react-app"
 
 function App() {
+
+  const [searchParams] = useSearchParams() // <-- get the "path"
+  const navigate = useNavigate()
+
+  useEffect(() => {
+    // check is there any redirect?
+    if (searchParams?.get('path')?.includes(baseName)) {
+      const redirectUrl = searchParams?.get('path')?.split('/').pop()
+
+      if (redirectUrl) {
+        navigate('/' + redirectUrl) // redirect
+      }
+    }
+  }, [])
+
   return (
     <Routes>
       <Route element={<Layout />}>
